65c9a04a267f287aff0c67eb5c817a205f32bdf3
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-18 12:46:22 +0530

    added function getWinner

--- index.js
+++ index.js
@@ -25,3 +25,28 @@ function getComputerChoice(){
         return 'scissor';
     }
 }
+function getWinner(p,c){
+    if (p === c){
+        return 'draw';
+    }else if (p === 'rock'){
+        if (c === 'paper'){
+            return 'computer';
+        }else{
+            return 'player';
+        }
+     }else if (p === 'paper'){
+         if (c === 'scissors') {
+             return 'computer';
+         }else{
+             return 'player';
+         }
+        }else if (p === 'scissors') {
+            if (c === 'rock') {
+                return 'computer';
+            }else{
+                return 'player';
+            }
+        }
+    }
+
+    
