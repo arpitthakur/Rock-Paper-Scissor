6ccf94f42418731c5eeba5afe6ce47dbbffff325
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-18 11:46:15 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const choices = document.querySelector('.choice');
+const score = document.querySelector('score');
+const result = document.querySelector('result');
+const restart = documemt.querySelector('restart');
+const model = document.querySelector('.model');
+
+const scoreboard = {
+    player:0,
+    computer:0
+};
 
